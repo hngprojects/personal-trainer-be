b8ec04fac293671fdad06ca5e51a745597e43f7c
Olatise Oluwatobiloba <olatise oluwatobiloba>
2026-06-04 09:32:42 +0100

    fix(bugs): fixed resource leaks, and other coderabbit suggested errs

--- cmd/createTrainers/main.go
+++ cmd/createTrainers/main.go
@@ -307,7 +307,6 @@ func createTrainerAcct(base_url string, client *http.Client, access_token string
 			}
 			appendIntoFailedTrainer(index, trainer, failedTrainers)
 			fmt.Printf("❌ failed to create trainer %v: receive status code: %v\n", trainer.Email, res.StatusCode)
-
 			continue
 		} else {
 			fmt.Printf("✅ Created trainer with email: %v\n", trainer.Email)
